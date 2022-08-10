39b1a0ff33ffb328558b181e415d8376b229bf2e
drony0610 <51791640+drony0610@users.noreply.github.com>
2022-08-10 12:28:12 +0300

    проверено

--- homework/src/App.js
+++ homework/src/App.js
@@ -2,9 +2,13 @@ import { Message } from './components/message/Message';
 import './App.css';
 
 function App() {
+  
+  //Добавлено преподавателем
+  const message = "Homework from lesson-1 is works!"
+  
   return (
     <div className="App">
-      <Message text="Homework from lesson-1 is works!"/>
+      <Message text={message}/>
     </div>
   );
 }
